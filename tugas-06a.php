ed01755047436e09de41117946e150fac4d898cd
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-09-27 14:37:26 +0700

    tugas_06_php
    ok

---
+++ tugas-06a.php
@@ -0,0 +1,27 @@
+<?php
+
+//deklarasi
+$x	= 4;
+$y	= 3;
+$z	= 21;
+
+$habib = $z;
+$aji = $x + $habib;
+$bintan = $aji + $y;
+
+
+
+
+//rumus
+
+echo "================================";
+echo "<br>";
+echo "Usia Habib = $habib";
+echo "<br>";
+echo "Usia Aji = $aji";
+echo "<br>";
+echo "Usia Bintan = $bintan";
+echo "<br>";
+echo "================================";
+
+?>
---
+++ tugas-06b.php
@@ -0,0 +1,29 @@
+<?php
+
+//deklarasi
+$a	= 5;
+$b 	= 6;
+$jambu = 15000;
+$dus   = 2000;
+
+$total_jambu = ($a * $b * $jambu);
+$total_dus	 = ($dus * 2);
+$jumlah = $total_jambu + $total_dus;		
+
+
+
+
+
+//rumus
+
+echo "================================";
+echo "<br>";
+echo "Harga Jambu = Rp. 15000 / Kg";
+echo "<br>";
+echo "Harga Kardus = Rp. 2000 / pcs";
+echo "<br>";
+echo "Total Penjualan (Dus dan Jambu) = Rp. <b>$jumlah</b>";
+echo "<br>";
+echo "================================";
+
+?>
